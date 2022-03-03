96580b95610dcdfdd9a5088e2f3e254a0231fd9f
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2022-03-04 04:05:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//Global variables
+var box;
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(600, 400);
+
+  //Creating a sprite
+  box = createSprite(300, 196, 20, 20);
+  box.shapeColor = "maroon";
+
+}
+
+function draw(){
+  background("black");
+
+  drawSprites();
+
+}
